45c520773c2af482eb82d171a0ddae410ee3cd96
Joe <joe2k01dev@gmail.com>
2017-05-01 14:18:15 +0200

    setup-makefiles.sh: add device string

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -15,6 +15,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
+DEVICE=super
 OUTDIR=vendor/$VENDOR/$DEVICE
 MAKEFILE=../../../$OUTDIR/$DEVICE-vendor-blobs.mk
 
